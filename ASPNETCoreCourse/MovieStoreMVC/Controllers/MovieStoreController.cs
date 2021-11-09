b92c468ec6694d8e4ab95bd8f7429748ce4e2d8a
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2021-11-09 13:50:44 +0100

    Edit Formular fertig

--- ASPNETCoreCourse/MovieStoreMVC/Controllers/MovieStoreController.cs
+++ ASPNETCoreCourse/MovieStoreMVC/Controllers/MovieStoreController.cs
@@ -56,5 +56,50 @@ namespace MovieStoreMVC.Controllers
         }
 
 
+        // GET: Movie/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movie/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Price,Genre")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+
     }
 }
