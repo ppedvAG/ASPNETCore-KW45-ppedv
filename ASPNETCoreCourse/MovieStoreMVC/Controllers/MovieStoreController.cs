8e73df9aea6923115df72012c4738fb58fdbdd22
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2021-11-09 11:54:47 +0100

    Create - Formular
    Details Formular

--- ASPNETCoreCourse/MovieStoreMVC/Controllers/MovieStoreController.cs
+++ ASPNETCoreCourse/MovieStoreMVC/Controllers/MovieStoreController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using MovieStoreMVC.Data;
 using System;
@@ -23,5 +24,37 @@ namespace MovieStoreMVC.Controllers
         {
             return View(await _context.Movies.ToListAsync());
         }
+
+        //Get-Methode: Details - Zeige einen Datensatz explizit an -> // GET: Movie/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound(); //NotFound -> 404 Http ErrorCode
+
+            Movie movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+            
+            if (movie == null)
+                return NotFound();
+          
+            return View(movie);
+        }
+
+
+       //Get-Methode -> Wir geben ein leeres Formular an den Browser
+       public IActionResult Create()
+       {
+            return View();
+       }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Movie movie) //Parameter-Binding wird hier in MVC relevant (Aus Formular wird ein Object)
+        {
+            _context.Add(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
